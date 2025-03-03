765eaf64b2329a7c17e9fa7c24343179f2626420
david-andreasson <79davand@gafe.molndal.se>
2025-03-03 09:42:05 +0100

    Trying to fix Cors error

--- src/main/java/com/davanddev/quizapp_api/config/WebConfig.java
+++ src/main/java/com/davanddev/quizapp_api/config/WebConfig.java
@@ -6,11 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")  // Allow all origins
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedOrigins("https://david-andreasson.github.io")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
