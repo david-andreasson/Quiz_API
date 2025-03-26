52894c74007b90de5820a29fb48c1fe196ef6f15
david-andreasson <79davand@gafe.molndal.se>
2025-03-26 15:16:07 +0100

    Attempting a git revert

--- src/main/java/com/davanddev/quizapp_api/models/UserEntity.java
+++ src/main/java/com/davanddev/quizapp_api/models/UserEntity.java
@@ -20,7 +20,8 @@ public class UserEntity {
     // Simple role management, e.g., "ROLE_USER"
     private String role;
 
-    public UserEntity() {}
+    public UserEntity() {
+    }
 
     public UserEntity(String username, String password, String role) {
         this.username = username;
@@ -32,24 +33,31 @@ public class UserEntity {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public String getRole() {
         return role;
     }
+
     public void setRole(String role) {
         this.role = role;
     }
