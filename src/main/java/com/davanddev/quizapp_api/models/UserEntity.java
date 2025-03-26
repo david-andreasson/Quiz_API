4929251f31bcca426a162f60b6aa92996177f391
david-andreasson <79davand@gafe.molndal.se>
2025-03-26 15:16:07 +0100

    Attempting a git revert

--- src/main/java/com/davanddev/quizapp_api/models/UserEntity.java
+++ src/main/java/com/davanddev/quizapp_api/models/UserEntity.java
@@ -20,55 +20,45 @@ public class UserEntity {
     // Simple role management, e.g., "ROLE_USER"
     private String role;
 
-    // New fields for first and last name
-    private String firstName;
-    private String lastName;
+    public UserEntity() {
+    }
 
-    public UserEntity() {}
-
-    public UserEntity(String username, String password, String role, String firstName, String lastName) {
+    public UserEntity(String username, String password, String role) {
         this.username = username;
         this.password = password;
         this.role = role;
-        this.firstName = firstName;
-        this.lastName = lastName;
     }
 
     // Getters and setters
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public String getRole() {
         return role;
     }
+
     public void setRole(String role) {
         this.role = role;
-    }
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 }
